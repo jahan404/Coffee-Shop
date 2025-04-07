98d57ab6fe53547e576afa88267fbec9906a37c8
Nusrat Jahan Raisa <jahanthinks@gmail.com>
2025-04-08 01:35:01 +0600

    banner image issue solved

--- src/Components/Banner.jsx
+++ src/Components/Banner.jsx
@@ -1,7 +1,7 @@
-
+// import banner from '../../public/banner.jpg'
 const Banner = () => {
     return (
-        <div className="bg-[url(../../public/banner.jpg)] min-h-[560px] bg-no-repeat bg-cover container mx-auto rounded-4xl">
+        <div className="bg-[url(/banner.jpg)] min-h-[560px] bg-no-repeat bg-cover container mx-auto rounded-4xl">
             
         </div>
     );
